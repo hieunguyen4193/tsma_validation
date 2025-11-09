d19c39cdcdc2b0fc2a784043cc4d796dcaac761b
Trong-Hieu Nguyen <work@hieunguyen.de>
2025-11-09 19:44:28 +0100

    update script to run 03

--- run_03.sh
+++ run_03.sh
@@ -4,7 +4,8 @@ all_samples=$(ls $inputdir/* -d);
 outputdir="/mnt/nvme/DATA_HIEUNGUYEN/outdir/TSMA_validation/fragmentomics_features";
 mkdir -p $outputdir;
 
-# for file in ${all_samples};do 
+for file in ${all_samples};do 
     # echo "Processing sample $file";
-    parallel -j 60 python 03_generate_fragmentomics_features.py -i {} -o ${outputdir} --fa ${path_to_all_fa} ::: ${all_samples}
-# done
+    # parallel -j 60 python 03_generate_fragmentomics_features.py -i {} -o ${outputdir} --fa ${path_to_all_fa} ::: ${all_samples}
+    python 03_generate_fragmentomics_features.py -i ${file} -o ${outputdir} --fa ${path_to_all_fa}
+done
